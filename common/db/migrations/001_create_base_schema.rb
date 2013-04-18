4b0a10d22b1c4d82cf2d4d5e569ab30cb64b4e0d
jambun <jambun@gmail.com>
2013-04-18 10:56:48 +1000

    added processing_funding_source to collection_management records

--- common/db/migrations/001_create_base_schema.rb
+++ common/db/migrations/001_create_base_schema.rb
@@ -1120,6 +1120,7 @@ Sequel.migration do
       TextField :processing_plan, :null => true
       Integer :processing_priority_id, :null => true
       Integer :processing_status_id, :null => true
+      TextField :processing_funding_source, :null => true
       TextField :processors, :null => true
       Integer :rights_determined, :default => 0, :null => false
 
